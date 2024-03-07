35fcb161e9fe9ca04674a54a845bb7972dd4fee4
Adriel <20230542@ricaldone.edu.sv>
2024-03-07 09:17:27 -0600

    header con javas

---
+++ Js/navbar.js
@@ -0,0 +1,26 @@
+function MenuPtc() {
+    const Menu = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Hospital</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Doctores</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Agendar citas</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>`;
+
+    return Menu;
+}
