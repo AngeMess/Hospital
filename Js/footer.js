8a0ebadbc81bc403a7e4760a95e5e52b9c9512f2
Adriel <20230542@ricaldone.edu.sv>
2024-03-20 22:06:31 -0600

    arreglo de errores varios

--- Js/footer.js
+++ Js/footer.js
@@ -3,8 +3,8 @@ function Footer() {
     <div class="container">
       <div class="footer-content">
         <h3>Contáctanos</h3>
-        <p>Correo: hospital_V@gmail.com</p>
-        <p>Teléfono: 123-456-789</p>
+        <p>Correo: medilife@gmail.com</p>
+        <p>Teléfono: 2340-4300</p>
       </div>
       <div class="footer-content">
         <h3>Paginas</h3>
